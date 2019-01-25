1b5e643e5e8066297292ef4377c2408aed8c5709
mehmenmike <mehmenmike@gmail.com>
2019-01-25 14:51:47 +0000

    document and add regex

--- src/ConfigFileLoader.java
+++ src/ConfigFileLoader.java
@@ -5,12 +5,33 @@ import java.util.Scanner;
 
 public class ConfigFileLoader {
 
-    public ArrayList<String> getFileContents(String filename) throws FileNotFoundException{
+    /**
+     * Takes a plaintext file and returns an ArrayList containing all the words and full stops in the file. Ensures
+     * that only words are permitted with the default regex string /(\$|£)?[\w']+/ and all letters are lowercased.
+     * Other regex strings are permitted.
+     *
+     * @param filename    the name of the file
+     * @param regexString the regex string to match
+     * @return an {@code ArrayList<String>} containing all the words in the text file, as well as full stops.
+     * @throws FileNotFoundException if the file is not found
+     */
+    public ArrayList<String> getFileContents(String filename, String regexString) throws FileNotFoundException{
         Scanner scanner = new Scanner(new File(filename));
         ArrayList<String> fileContents = new ArrayList<>();
 
+        String currentLine;
+
         while(scanner.hasNext()){
-            fileContents.add(scanner.nextLine());
+            currentLine = scanner.nextLine();
+
+            for(String currentWord : currentLine.split(" ")){
+                if(currentWord.matches("(\\$|£)?[\\w']+")){
+                    fileContents.add(currentWord);
+                } else if(currentWord.matches("(\\$|£)?[\\w']+.")){
+                    fileContents.add(currentWord.substring(0, currentWord.length() - 1));
+                    fileContents.add(".");
+                }
+            }
         }
 
         return fileContents;
