07a00c10199081fed81d001ccf586a71cb215369
mehmenmike <mehmenmike@gmail.com>
2019-01-26 12:55:30 +0000

    add default regex and document

--- src/ConfigFileLoader.java
+++ src/ConfigFileLoader.java
@@ -6,9 +6,22 @@ import java.util.Scanner;
 public class ConfigFileLoader {
 
     /**
-     * Takes a plaintext file and returns an ArrayList containing all the words and full stops in the file. Ensures
-     * that only words are permitted with the default regex string /(\$|£)?[\w']+/ and all letters are lowercased.
-     * Other regex strings are permitted.
+     * Takes a plaintext file and returns an ArrayList containing all the words and full stops in the file. This
+     * method splits the contents of the file by spaces, then checks each element against the default regex string
+     * "(\\$|£)?[\\w']+". If a complete match is found, the word is accepted. All letters are lowercased. If a word
+     * has a comma or a full stop immediately following it, then the word is stripped of the punctuation point and
+     * the point is entered as its own word.
+     */
+    public ArrayList<String> getFileContents(String filename) throws FileNotFoundException{
+        return this.getFileContents(filename, "(\\$|£)?[\\w']+");
+    }
+
+    /**
+     * Takes a plaintext file and returns an ArrayList containing all the words and full stops in the file. This
+     * method splits the contents of the file by spaces, then checks each element against a supplied regex string. If
+     * a complete match is found, the word is accepted. All letters are lowercased. If a word has a comma or a full
+     * stop immediately following it, then the word is stripped of the punctuation point and the point is entered as
+     * its own word. Thus, do not include any of [,.] in your regex string.
      *
      * @param filename    the name of the file
      * @param regexString the regex string to match
@@ -25,12 +38,12 @@ public class ConfigFileLoader {
             currentLine = scanner.nextLine().toLowerCase();
 
             for(String currentWord : currentLine.split(" ")){
-                if(currentWord.matches("(\\$|£)?[\\w']+")){
+                if(currentWord.matches(regexString)){
                     fileContents.add(currentWord);
-                } else if(currentWord.matches("(\\$|£)?[\\w']+\\.")){
+                } else if(currentWord.matches(regexString + "\\.")){
                     fileContents.add(currentWord.substring(0, currentWord.length() - 1));
                     fileContents.add(".");
-                } else if(currentWord.matches("(\\$|£)?[\\w']+,")){
+                } else if(currentWord.matches(regexString + ",")){
                     fileContents.add(currentWord.substring(0, currentWord.length() - 1));
                     fileContents.add(",");
                 }
