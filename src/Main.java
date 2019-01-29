a68384f3c68f451a41bc1b475c5d91760b2d6f3a
mehmenmike <mehmenmike@gmail.com>
2019-01-29 22:03:46 +0000

    never used this

--- src/Main.java
+++ src/Main.java
@@ -47,7 +47,6 @@ public class Main {
         MarkovPoint currentPoint = markovPoints.get(random.nextInt(markovPoints.size()));
         MarkovPoint nextPoint;
         String outputWord;
-        String nextWord;
 
         while(true){
             outputWord = currentPoint.getSource();
