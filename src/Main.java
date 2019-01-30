9acd76739414f614ed9e846f72f184427d202eb9
mehmenmike <mehmenmike@gmail.com>
2019-01-30 01:26:07 +0000

    beginnings of a proper GUI

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
-import java.io.FileNotFoundException;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
     public static void main(String[] args){
-        // TODO: Capitalise the start of sentences
 
-        MarkovChainController markovChainController;
-        try {
-            markovChainController = new MarkovChainController("src/sampletext");
-        } catch(FileNotFoundException e) {
-            return;
-        }
+        JFrame jFrame = new JFrame("ChatterBot");
+        jFrame.setLayout(new GridLayout(3, 1));
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(600, 500);
+        jFrame.setResizable(false);
 
-        System.out.println(markovChainController.generateText(100));
+        JTextArea inputArea = new JTextArea();
+
+        JButton activateButton = new JButton("Go!");
+
+        JTextArea outputArea = new JTextArea();
+        outputArea.setEditable(false);
+
+        jFrame.add(inputArea);
+        jFrame.add(activateButton);
+        jFrame.add(outputArea);
+
+        activateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                TextLoaderFormatter.splitText(inputArea.getText());
+            }
+        });
+
+        jFrame.setVisible(true);
+
+
     }
 }
